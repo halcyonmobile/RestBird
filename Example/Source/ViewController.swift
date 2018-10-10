292ba8659a88cd141815d705c6f04aad77c1711c
Botond Magyarosi <mboti@halcyonmobile.com>
2018-10-10 09:44:07 +0300

    Add Example project (part 1)

---
+++ Example/Source/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS Example
+//
+//  Created by Botond Magyarosi on 09/10/2018.
+//  Copyright © 2018 Botond Magyarosi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
