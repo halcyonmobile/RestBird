7c298f4891d5a9c4f019170cf281ea70a9a11b6b
Tamas Levente <tamas.levente@lego.com>
2018-10-29 11:29:24 +0200

    Create typealiases for Request headers and parameters. Move parameters into base Request

--- Sources/RestBird/Request/Request.swift
+++ Sources/RestBird/Request/Request.swift
@@ -7,6 +7,9 @@
 
 import Foundation
 
+public typealias RequestHeaders     = [String: Any]
+public typealias RequestParameters  = [String: Any]
+
 /// Abstract REST request protocol.
 public protocol Request {
     associatedtype ResponseType: Decodable
@@ -17,8 +20,12 @@ public protocol Request {
     /// The url suffix of the request. Default: `nil`
     var suffix: String? { get }
 
-    /// Additional headers fir the request. Default: `nil`.
-    var headers: [String: Any]? { get }
+    /// Additional headers for the request. Default: `nil`.
+    var headers: RequestHeaders? { get }
+
+    /// The parameter dictionary of the request. Default: nil
+    var parameters: RequestParameters? { get }
+
 }
 
 extension Request {
@@ -27,5 +34,7 @@ extension Request {
     
     public var suffix: String? { return nil }
 
-    public var headers: [String: Any]? { return nil }
+    public var headers: RequestHeaders? { return nil }
+
+    public var parameters: RequestParameters? { return nil }
 }
